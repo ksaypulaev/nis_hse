9691db5dcc5ab7220d79f18a7109edcae15dbd32
Kamil Saypulaev <kamil.saypulaev@gmail.com>
2023-11-20 21:31:08 +0300

    Exception Handling

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
 import pandas as pd
 # import numpy as np
 
-pd.read_csv('plavki.csv', delimiter=',')
+plavki = pd.read_csv('plavki.csv', delimiter=',')
+ostatki = pd.read_csv('ostatki.csv', delimiter=',')
+try:
+    print(f"{plavki}")
+    print('-' * 100)
+    print(ostatki)
+except Exception as ex:
+    print(f'An exception occured: {ex}')
+else:
+    print("\nThe program finished successfuly!")
